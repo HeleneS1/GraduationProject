137ce79eab71ab5670b49e27f41432baec55fae9
Lene <Lene.rossland@gmail.com>
2020-10-15 14:00:07 +0200

    InsertIntoAccidentStage

--- Domains/to_staging_area.py
+++ Domains/to_staging_area.py
@@ -1,5 +1,3 @@
-""" This file takes a csv and adds the info to the staging area"""
-
 
 import psycopg2
 import pandas as pd
@@ -17,6 +15,20 @@ hospitals = sykehus[['Id',
 hospitals.rename(columns={'Id':'hosp_id', 'Sykehus':'hosp_name', 'Kommune':'hosp_city', 'Lat':'hosp_lat', 
                           'Long':'hosp_long'}, inplace=True)
 
+accidents = pd.read_csv('MVP_ulykker.csv', delimiter=',', encoding='iso-8859-1')
+accidents.columns
+acc_new = accidents[['id',
+                     'url',
+                     'ost_coord',
+                     'nord_coord',
+                     'antall drepte',
+                     'dato'
+                     
+                     ]]
+acc_new.columns
+acc_new.rename(columns={'id':'acc_id', 'url':'url',
+                          'ost_coord':'loc_lat', 'nord_coord':'loc_long', 'antall drepte':'death', 'dato': 'date'}, inplace=True)
+
 
 #Populate hospital
 # hostname = "ds-etl-academy.cgbivchwjzle.eu-west-1.rds.amazonaws.com"
@@ -28,11 +40,14 @@ hospitals.rename(columns={'Id':'hosp_id', 'Sykehus':'hosp_name', 'Kommune':'hosp
 # hospitals.to_sql('d_hospital', engine, index=False, schema='public', if_exists='append', method='multi', chunksize=1000)
 
 #populate as a function. 
-def populate(username, password_name, tablename):
+def populate(username, password_name, df, tablename):
     hostname = "ds-etl-academy.cgbivchwjzle.eu-west-1.rds.amazonaws.com"
     user = username
     password = password_name
     dbname = "gp_lama"
     constring = f"postgresql+psycopg2://{user}:{password}@{hostname}/{dbname}"
     engine = create_engine(constring, echo=False)  
-    hospitals.to_sql(tablename, engine, index=False, schema='public', if_exists='append', method='multi', chunksize=1000)
+    df.to_sql(tablename, engine, index=False, schema='public', if_exists='append', method='multi', chunksize=1000)
+
+
+populate('student_lene', 'osama', acc_new, 'accident_stage')
